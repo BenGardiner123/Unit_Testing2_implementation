9b7b0ea9220e068a531501739789266fe6833d50
SamualBanfield <samualbanfield@gmail.com>
2020-08-17 17:02:01 +1000

    Finalised Initial Tests

--- MiscLib/Group1.cs
+++ MiscLib/Group1.cs
@@ -2,33 +2,39 @@
 
 namespace MiscLib
 {
-    public class GroupOneFunctions
+  public class GroupOneFunctions
+  {
+    //---- Group 1 Functions ----------
+    public int CalcRectArea(int length, int width)
     {
-        //---- Group 1 Functions ----------
-        public int CalcRectArea(int length, int width) {
-            throw new NotImplementedException();
-        }
-
-        public bool IsFactiorialNum(int num) {
-            throw new NotImplementedException();
-        }
-
-        public int CelciusToFarenheit(int celcius) {
-            throw new NotImplementedException();
-        }
-
-        public bool IsPalindrome(string word) {
-            throw new NotImplementedException();
-        }
-
-        public string StringReplace(string word, char replacement) {
-            throw new NotImplementedException();
-        }
-
-        public int CharCount(string word, char countCharacter) {
-            throw new NotImplementedException();
-        }
-
-        
+      throw new NotImplementedException();
     }
+
+    public bool IsFactiorialNum(int num)
+    {
+      throw new NotImplementedException();
+    }
+
+    public int CelciusToFarenheit(int celcius)
+    {
+      throw new NotImplementedException();
+    }
+
+    public bool IsPalindrome(string word)
+    {
+      throw new NotImplementedException();
+    }
+
+    public string StringReplace(string word, char replacement)
+    {
+      throw new NotImplementedException();
+    }
+
+    public int CharCount(string word, char countCharacter)
+    {
+      throw new NotImplementedException();
+    }
+
+
+  }
 }
---
+++ Tests/UnitTesting.cs
@@ -0,0 +1,79 @@
+using System;
+using Xunit;
+using MiscLib;
+
+namespace Test
+{
+  public class UnitTesting
+  {
+    [Theory]
+    [InlineData(3, 4, 12)]
+    [InlineData(2, -4, 8)] //Negative Check
+    [InlineData(0, -4, 8)] //Zero Check
+    public void CalcRectArea_Test(int length, int width, int result)
+    {
+      var test = new GroupOneFunctions();
+      //Negative numbers produce a exception
+      if (length < 0 || width < 0)
+      {
+        Assert.Throws<Exception>(() => test.CalcRectArea(length, width));
+      }
+      else
+      {
+        Assert.Equal(result, test.CalcRectArea(length, width));
+      }
+    }
+
+    [Theory]
+    [InlineData(21, true)]
+    [InlineData(-6, false)]
+    [InlineData(6, false)]
+    public void IsFactiorialNum_Test(int num, bool result)
+    {
+      var test = new GroupOneFunctions();
+      Assert.Equal(result, test.IsFactiorialNum(num));
+    }
+
+    [Theory]
+    [InlineData(13, 55.4)]
+    [InlineData(-13, 8.6)]
+    public void CelciusToFarenheit_Test(int num, float result)
+    {
+      var test = new GroupOneFunctions();
+      Assert.Equal(result, test.CelciusToFarenheit(num));
+    }
+
+    [Theory]
+    [InlineData("kayak", true)]
+    [InlineData("Kayak", true)] //Capitalisation
+    [InlineData("yo banana boy", true)] //Spaces
+    [InlineData("Palindrome", false)]
+    public void IsPalindrome_Test(string str, bool result)
+    {
+      var test = new GroupOneFunctions();
+      Assert.Equal(result, test.IsPalindrome(str));
+    }
+
+    [Theory]
+    [InlineData("this is a string", 'i', 'x', "thxs xs a strxng")]
+    [InlineData("thIs is a strIng", 'i', 'x', "thxs xs a strxng")] //Capitalisation.
+    [InlineData("this is a string", 'w', 'x', "this is a string")]
+    [InlineData("", 'i', 'x', "")]
+    public void StringReplace_Test(string str, char targetChar, char replacementChar, string result)
+    {
+      var test = new GroupOneFunctions();
+      Assert.Equal(result, test.StringReplace(str, targetChar, replacementChar));
+    }
+
+    [Theory]
+    [InlineData("this is a string", 'i', 3)]
+    [InlineData("thIs is a strIng", 'i', 3)] //Capitalisation.
+    [InlineData("this is a string", 'w', 0)]
+    [InlineData("", 'i', 0)] //String with 0 length.
+    public void CharCount_Test(string str, char character, int result)
+    {
+      var test = new GroupOneFunctions();
+      Assert.Equal(result, test.CharCount(str, character));
+    }
+  }
+}
